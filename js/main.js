d7ce974fb999d45453319bec5e6750341550edd2
James Cortés <james.cortes@banchile.cl>
2016-12-18 23:05:50 -0300

    Se argega integración con Firebase, pero falta configurar el acceso.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module("simpleApp", ["ngRoute", 'ui.materialize']);
+var app = angular.module("simpleApp", ["ngRoute", "ui.materialize", "angular-loading-bar", "ngAnimate", "firebase"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
@@ -22,8 +22,59 @@ app.controller('demoCtrl', ["$scope", function ($scope) {
             choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
         };
 
-        $scope.mostrarConsola = function (){
-            console.log("Hola soy un texto en la consola");
-        };
+        $
 
       }]);
+
+
+app.controller('MainCtrl', function($scope, $firebaseArray) {
+
+
+
+        $scope.mostrarConsola = function (){
+            console.log("Hola soy un texto en la consola");
+
+        };
+
+        $scope.searchCode = function (code){
+
+          var refTypeInfo = firebase.database().ref().child("typeInfo");
+          // create a synchronized array to typeInfo
+          $scope.typeInfos = $firebaseArray(refTypeInfo);
+
+          //TODO: static to variable
+          $scope.typeInfo = 'phone';
+
+          try{
+          //if($scope.firebaseUser){
+            if(code){
+              var refCodes = firebase.database().ref().child("codes").orderByChild("codeInfo").equalTo(code);
+            }else{
+              var refCodes = firebase.database().ref().child("codes");
+            }
+
+            // create a synchronized array to codes
+            $scope.codes = $firebaseArray(refCodes);
+           //}
+
+        }catch(err){
+              console.log(err);
+        }
+      };
+
+      $scope.addInfo = function() {
+
+        var refCodesAdd = firebase.database().ref().child("codes");
+        // create a synchronized array to codes
+        $scope.codesAdd = $firebaseArray(refCodesAdd);
+        $scope.codesAdd.$add({
+              msgInfo: $scope.msgInfo,
+              typeInfo: $scope.typeInfo,
+              user: $scope.firebaseUser.uid,
+              codeInfo: $scope.codeInfo,
+              timestamp: firebase.database.ServerValue.TIMESTAMP
+        });
+      };
+
+
+      });
